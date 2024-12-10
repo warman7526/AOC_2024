8ca4b1b2821db391d2e33853811c245383f72ef9
warman7526 <warman7526@bradfordgrammar.com>
2024-12-10 21:41:57 +0000

    day5 that wont destroy my PC
    optimised so it wasnt checking 2.07e+50  different lists for validity :p

--- day5/app_2.py
+++ day5/app_2.py
@@ -1,6 +1,3 @@
-##THIS WILL EVENTUALLY WORK, BUT NGL PROBABLY AFTER THE HEAT DEATH OF THE UNIVERSE :p
-##I WILL PROBABLY FIX AT SOME POINT BUT I HAVE NO TIME RN lol
-
 import itertools, math
 
 with open("day5/data.txt", "r") as f:
@@ -11,7 +8,8 @@ updates = [update.split(",") for update in data[1].split("\n")]
 
 
 def is_valid(i, update):
-    print("validating update", i)
+    if i != -1:
+        print("validating update", i)
     valid_rules = [rule for rule in rules if (rule[0] in update and rule[1] in update)]
     positions = {}
     for page in update:
@@ -28,18 +26,31 @@ def middle_page(update):
     return int(update[len(update) // 2])
 
 
-def corrected(i, update):
-    print("correcting update", i)
-    permutations = itertools.permutations(update)
-    for j in range(math.factorial(len(update))):
-        if is_valid(str(i) + "." + str(j), t := next(permutations)):
-            return t
-    return None
+def correct(n, update):
+    print("correcting update", n)
+
+    def correction_step(placed: list, remaining: list):
+        for i, item in enumerate(remaining):
+            for pos in range(len(placed) + 1):
+                test = placed.copy()
+                test.insert(pos, item)
+                if is_valid(-1, test):
+                    if len(test) == len(update):
+                        return test
+                    next = correction_step(test, remaining[:i] + remaining[i+1:])
+                    if next is not None:
+                        return next
+
+    return correction_step([], update)
 
 
-corrected_updates = [
-    corrected(i, update) for i, update in enumerate(updates) if not is_valid(i, update)
-]
+corrected_updates = [correct(i, update) for i,update in enumerate(updates) if not is_valid(i, update)]
+
 
 middle_pages = [middle_page(update) for update in corrected_updates]
 print(f"{middle_pages=}\n{sum(middle_pages)=}")
+
+checks = 0
+for update in corrected_updates:
+    checks += math.factorial(len(update))
+print(math.log(checks))
